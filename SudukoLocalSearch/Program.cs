f24e39827a08f857a24ad3b9de07e726f3ba4c83
ThomasZwart <36655280+ThomasZwart@users.noreply.github.com>
2018-09-07 13:55:41 +0200

    Initial Commit

---
+++ SudukoLocalSearch/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Threading;
+
+namespace SudukoLocalSearch
+{
+    class Program
+    {
+        private static readonly Random getRandom = new Random();
+
+        static void Main()
+        {
+            int[,] input = ReadSudoku();
+
+            long x = DateTime.Now.Ticks;
+
+            Sudoku sudoku = new Sudoku(input);
+            Console.WriteLine();
+            Console.WriteLine("Randomly filled the blanks:");
+            sudoku.WriteSudoku();
+            Console.WriteLine("Evaluation: " + sudoku.GetEvaluationValue());
+
+            Sudoku newsudoku = HillClimbing(sudoku);
+            Console.WriteLine();
+            Console.WriteLine("Solution:");
+            newsudoku.WriteSudoku();
+            Console.WriteLine("Evaluation: " + newsudoku.GetEvaluationValue());
+
+            Console.WriteLine("Time Elapsed (in seconds): " + TimeSpan.FromTicks(DateTime.Now.Ticks - x).TotalSeconds);
+
+            Console.ReadLine();
+        }
+
+        private static Sudoku HillClimbing(Sudoku sudoku)
+        {
+            int blockLength = (int)Math.Sqrt(sudoku.Length); // For a 9x9 sudoku the blocklength is 3
+            int equalCounter = 0; // Counts how many times the swap resulted in an equal score
+
+            // Parameters
+            int progressTolerance = 100; // After this many steps no progress --> randomwalk
+            int randomWalkSteps = 10; // Amount of randomwalk steps
+
+            // Choose a block
+            int blockColumn = GetRandomNumber(0, blockLength); // For a 9x9 sudoku, the randoms can be 0, 1 or 2 with blocklength = 3
+            int blockRow = GetRandomNumber(0, blockLength);
+            int bestValue = sudoku.GetEvaluationValue();
+
+            Sudoku bestState = sudoku; // To store states before random-walking
+
+            Sudoku newSudoku = GetBestSwap(sudoku, blockRow, blockColumn);
+
+            while (newSudoku.GetEvaluationValue() != 0)
+            {
+                sudoku = newSudoku;
+
+                blockRow = GetRandomNumber(0, blockLength);
+                blockColumn = GetRandomNumber(0, blockLength);
+
+                newSudoku = GetBestSwap(sudoku, blockRow, blockColumn); // Is always better than sudoku
+
+                // No progress
+                if (sudoku.GetEvaluationValue() == newSudoku.GetEvaluationValue())
+                {
+                    equalCounter++;
+                    // If there is no progress for more steps then the tolerance or there is a local optimum
+                    if (equalCounter > progressTolerance)
+                    {
+                        // Go back to the best state we had before random walking
+                        if (bestValue < newSudoku.GetEvaluationValue())
+                            newSudoku = bestState;
+
+                        bestValue = newSudoku.GetEvaluationValue();
+                        bestState = newSudoku;
+                        newSudoku = RandomWalk(randomWalkSteps, newSudoku);
+                        equalCounter = 0;
+                    }
+                }
+            }
+            return newSudoku;
+        }
+
+        private static Sudoku GetBestSwap(Sudoku sudoku, int blockRow, int blockColumn)
+        // Finds the best possible swap (or if there is no improving swap it returns a clone of the input)
+        {
+            int blockLength = (int)Math.Sqrt(sudoku.Length); // For a 9x9 sudoku the blocklength is 3
+
+            // Get the starting indices of the block
+            int blockIndexColumn = blockColumn * blockLength;
+            int blockIndexRow = blockRow * blockLength;
+
+            int bestValue = sudoku.GetEvaluationValue(); // The best value thusfar is the current value
+            Sudoku bestState = sudoku.Clone(); // Best state is the current state
+
+            for (int row = blockIndexRow; row < blockIndexRow + blockLength; row++) // For every row in the randomly chosen block
+            {
+                for (int column = blockIndexColumn; column < blockIndexColumn + blockLength; column++) // And every column
+                {
+                    // Swap with all the other numbers in the block, not including the ones that already has been swapped.
+                    // For a 9x9 sudoku there will be 45 possible swaps (if you also swap a number with itself)
+                    for (int y = row; y < blockIndexRow + blockLength; y++)
+                    {
+                        // So that no number gets swapped with itself or a number that it has already been swapped with
+                        int q = blockIndexColumn;
+                        if (y == row)
+                            q = column + 1;
+
+                        for (int x = q; x < blockIndexColumn + blockLength; x++)
+                        {
+                            // Only swap non-fixed numbers
+                            if (!sudoku.fixedNumbers.Contains(new Tuple<int, int>(row, column)) &&
+                                !sudoku.fixedNumbers.Contains(new Tuple<int, int>(y, x)))
+                            {
+                                sudoku.Swap(row, column, y, x); // Swap two numbers
+                                if (sudoku.GetEvaluationValue() <= bestValue) // If the swap resulted in a better (or equal) value
+                                {
+                                    Sudoku state = sudoku.Clone(); // Clone that sudoku (with the swap)
+                                    bestValue = sudoku.GetEvaluationValue(); // The bestvalue is now this new value                                     
+                                    bestState = state; // The best state is now this new sudoku
+                                }
+                                sudoku.Swap(row, column, y, x); // Swap back to not keep original sudoku intact
+                            }
+                        }
+                    }
+                }
+            }
+            return bestState;
+        }
+
+        private static Sudoku RandomWalk(int steps, Sudoku sudoku)
+        {
+            int blockLength = (int)Math.Sqrt(sudoku.Length);
+
+            Sudoku newSudoku = sudoku.Clone();
+            for (int i = 0; i < steps; i++)
+            {
+                // Get a random block
+                int blockRow = GetRandomNumber(0, blockLength);
+                int blockColumn = GetRandomNumber(0, blockLength);
+
+                // Get 2 random numbers by index from that block
+                int randomRow1 = GetRandomNumber(blockRow * blockLength, blockRow * blockLength + blockLength);
+                int randomColumn1 = GetRandomNumber(blockColumn * blockLength, blockColumn * blockLength + blockLength);
+                int randomRow2 = GetRandomNumber(blockRow * blockLength, blockRow * blockLength + blockLength);
+                int randomColumn2 = GetRandomNumber(blockColumn * blockLength, blockColumn * blockLength + blockLength);
+
+                // If the points are not fixed
+                if (!newSudoku.fixedNumbers.Contains(new Tuple<int, int>(randomRow1, randomColumn1))
+                    && !newSudoku.fixedNumbers.Contains(new Tuple<int, int>(randomRow2, randomColumn2)))
+                {
+                    // Swap them
+                    newSudoku.Swap(randomRow1, randomColumn1, randomRow2, randomColumn2);
+                }
+                else
+                {
+                    // Because there is no swap, increase steps to make sure the amount of swaps will be equal to steps;
+                    steps++;
+                }
+            }
+            return newSudoku;
+        }
+
+        private static int[,] ReadSudoku()
+        {
+            try
+            {
+                // Get the length of the first row N, the sudoku size is N * N 
+                char[] firstRowArray = Console.ReadLine().Trim().ToCharArray();
+                
+                int sudokuLength = firstRowArray.Length;
+                int[,] sudoku = new int[sudokuLength, sudokuLength];
+                string[] allRows = new string[sudokuLength];
+
+                // Add the first row to the sudoku array
+                for (int i = 0; i < firstRowArray.Length; i++)
+                {
+                    sudoku[0, i] = int.Parse(firstRowArray[i].ToString());
+                }
+
+                // Add the rest of the rows to the sudoku array
+                for (int i = 1; i < sudokuLength; i++)
+                {
+                    string s = Console.ReadLine().Trim();
+                    char[] rowCharArray = s.ToCharArray();
+
+                    for (int j = 0; j < rowCharArray.Length; j++)
+                    {
+                        sudoku[i, j] = int.Parse(rowCharArray[j].ToString());
+                    }
+                }
+                return sudoku;
+            }
+            catch (Exception e)
+            {
+                // Incorrect input
+                Console.WriteLine(e.Message);
+                Thread.Sleep(3000); // Wait for user to read error
+                Environment.Exit(0); // Exit console
+                return null;
+            }
+        }
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getRandom)
+            {
+                return getRandom.Next(min, max);
+            }
+        }
+    }
+}
---
+++ SudukoLocalSearch/Sudoku.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SudukoLocalSearch
+{
+    class Sudoku
+    {
+        public int[,] grid;
+        public HashSet<int>[] rowsUniques;
+        public HashSet<int>[] columnUniques;
+        public HashSet<Tuple<int, int>> fixedNumbers;
+
+        private static readonly Random getRandom = new Random();
+
+        public Sudoku(int[,] _sudoku)
+        {
+            grid = _sudoku;
+            SetFixedNumbers();
+            RandomFillSudoku();
+            PopulateHashSets();
+        }
+
+        public void Swap(int row1, int column1, int row2, int column2)
+        {
+            // Swaps two numbers
+            int temp = grid[row1, column1];
+            grid[row1, column1] = grid[row2, column2];
+            grid[row2, column2] = temp;
+
+            // The 4 hashsets corresponding get cleared
+            rowsUniques[row1].Clear();
+            rowsUniques[row2].Clear();
+            columnUniques[column1].Clear();
+            columnUniques[column2].Clear();
+
+            // And refilled
+            for (int i = 0; i < Length; i++)
+            {
+                rowsUniques[row1].Add(grid[row1, i]);
+                rowsUniques[row2].Add(grid[row2, i]);
+                columnUniques[column1].Add(grid[i, column1]);
+                columnUniques[column2].Add(grid[i, column2]);
+            }
+        }
+
+        public Sudoku Clone() // Clones the current sudoku
+        {
+            // The grid cloned
+            Sudoku clone = new Sudoku(grid.Clone() as int[,])
+            // A new array for the hashsets
+            {
+                columnUniques = columnUniques.Clone() as HashSet<int>[],
+                rowsUniques = rowsUniques.Clone() as HashSet<int>[]
+            };
+
+            // All hashsets cloned in arrays
+            for (int i = 0; i < Length; i++)
+            {
+                clone.columnUniques[i] = new HashSet<int>(columnUniques[i]);
+                clone.rowsUniques[i] = new HashSet<int>(rowsUniques[i]);
+            }
+            
+            clone.fixedNumbers = new HashSet<Tuple<int, int>>(fixedNumbers);       
+            return clone;
+        }
+
+        public int this[int index1, int index2] // Used to access the grid variable easily
+        {
+            get { return grid[index1, index2]; }
+            
+            set { grid[index1, index2] = value; }
+        }
+
+        public void SetFixedNumbers() // Sets all numbers that aren't blank as fixed numbers in the sudoku based on their indices                                                   
+        {
+            fixedNumbers = new HashSet<Tuple<int, int>>();
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++)
+                {
+                    if (grid[i,j] != 0)
+                    {
+                        fixedNumbers.Add(new Tuple<int, int>(i, j)); // Tuple of (row, column) coordinates in the grid
+                    }
+                }
+            }
+        }
+
+        public void PopulateHashSets() // Populates the hashsets for every column and row, to see how many unique numbers there are
+        {
+            columnUniques = new HashSet<int>[Length];
+            rowsUniques = new HashSet<int>[Length];
+
+            for (int i = 0; i < Length; i++) // Fills the hashset for every column and row
+            {
+                columnUniques[i] = new HashSet<int>();
+                rowsUniques[i] = new HashSet<int>();
+                for (int j = 0; j < Length; j++)
+                {
+                    columnUniques[i].Add(grid[j, i]);
+                    rowsUniques[i].Add(grid[i, j]);
+                }
+            }
+        }
+
+        public int Length // The length of the sudoku
+        {
+            get { return grid.GetLength(0); }
+        }
+
+        public int GetEvaluationValue()
+        {
+            int evaluationValue = 0;
+
+            for (int i = 0; i < Length; i++)
+            {
+                // The amount of possible unique numbers minus the unique numbers in that column/row in equal to the amount of missing numbers
+                evaluationValue += Length - columnUniques[i].Count;
+                evaluationValue += Length - rowsUniques[i].Count;
+            }
+            return evaluationValue;
+        }
+
+        public void WriteSudoku()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void RandomFillSudoku() // Randomly fills all the blank spaces in the sudoku so that all possible numbers appear once in every block
+        {
+            int blockLength = (int)Math.Sqrt(Length);
+            List<int> numberAvailable = new List<int>();
+
+            for (int i = 0; i < blockLength; i++)
+            {
+                for (int j = 0; j < blockLength; j++)
+                {
+                    // Stores the available numbers so 9 different random numbers will get chosen each block
+                    for (int l = 1; l <= Length; l++)
+                    {
+                        numberAvailable.Add(l);
+                    }
+
+                    // Every fixed number gets removed from the available numbers
+                    for (int y = i * blockLength; y < i * blockLength + blockLength; y++)
+                    {
+                        for (int x = j * blockLength; x < j * blockLength + blockLength; x++)
+                        {
+                            if (grid[y, x] != 0) 
+                            {
+                                numberAvailable.Remove(grid[y, x]);
+                            }
+                        }
+                    }
+
+                    // The rest of the blank spaces in this block get randomly filled with the available numbers
+                    for (int y = i * blockLength; y < i * blockLength + blockLength; y++)
+                    {
+                        for (int x = j * blockLength; x < j * blockLength + blockLength; x++)
+                        {
+                            if (grid[y, x] == 0) 
+                            {
+                                int r = GetRandomNumber(0, numberAvailable.Count); // Get a random number from available numbers
+                                grid[y, x] = numberAvailable[r];
+                                numberAvailable.Remove(grid[y,x]); // The number is now unavailable in this block
+                            }
+                        }
+                    }
+                    numberAvailable.Clear();
+                }
+            }
+        }
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getRandom)
+            {
+                return getRandom.Next(min, max);
+            }
+        }
+    }
+}
